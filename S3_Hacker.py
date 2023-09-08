35c1418a5c67722cbb74af5c0045376a43606004
KarAshutosh <karanamashutosh@gmail.com>
2023-09-08 06:20:47 +0530

    New commit

--- S3_Hacker.py
+++ S3_Hacker.py
@@ -2,12 +2,12 @@ import subprocess
 
 
 ## Single bucket
-# option = 1
+option = 1
 # Replace with your S3 bucket name
 bucket_name = "ak-s3-testing" 
 
 ## Array of bucket names 
-option = 2
+# option = 2
 # Replace with your S3 bucket names
 bucket_names = ["ak-s3-testing", "ak-s3-testing"]
 
@@ -23,7 +23,6 @@ allow_rm = False
 def no_sign_list_s3_bucket(bucket_name):
     print("Running the function no_sign_list_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 ls s3://{bucket_name} --no-sign-request"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -35,13 +34,11 @@ def no_sign_list_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def list_s3_bucket(bucket_name):
     print("Running the function list_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 ls s3://{bucket_name}"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -53,13 +50,11 @@ def list_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def no_sign_mv_s3_bucket(bucket_name):
     print("Running the function no_sign_mv_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 mv hi.txt s3://{bucket_name}/hi.txt --no-sign-request"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -71,13 +66,11 @@ def no_sign_mv_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def mv_s3_bucket(bucket_name):
     print("Running the function mv_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 mv hi.txt s3://{bucket_name}/hi.txt"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -89,13 +82,11 @@ def mv_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def no_sign_cp_s3_bucket(bucket_name):
     print("Running the function no_sign_cp_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 cp hi.txt s3://{bucket_name} --no-sign-request"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -107,13 +98,11 @@ def no_sign_cp_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def cp_s3_bucket(bucket_name):
     print("Running the function cp_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 cp hi.txt s3://{bucket_name}"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -125,13 +114,11 @@ def cp_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def no_sign_rm_s3_bucket(bucket_name):
     print("Running the function no_sign_rm_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 rm s3://{bucket_name}/hi.txt --no-sign-request"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -143,13 +130,11 @@ def no_sign_rm_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 def rm_s3_bucket(bucket_name):
     print("Running the function rm_s3_bucket()")
     try:
-        # Use subprocess to run the AWS CLI command
         cmd = f"aws s3 rm s3://{bucket_name}/hi.txt"
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Command Output (stdout):")
@@ -161,7 +146,6 @@ def rm_s3_bucket(bucket_name):
         print(f"Command failed with error code {e.returncode}:")
         print(e.stderr.decode('utf-8'))
     except Exception as e:
-        # Handle other exceptions
         print(f"An error occurred: {str(e)}")
 
 
@@ -169,7 +153,6 @@ def read_names_from_file(file_path):
     try:
         with open(file_path, 'r') as file:
             names = file.read().split('\n')
-            # Remove any empty strings
             names = [name.strip() for name in names if name.strip()]
         return names
     except FileNotFoundError:
